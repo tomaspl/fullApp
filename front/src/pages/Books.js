beaa518ed14441c894a0771a5e75972d1ef40263
Tomas Leiva <tleiva@globaltask.net>
2022-02-09 09:38:08 -0500

    Creation of searcher of books

--- front/src/pages/Books.js
+++ front/src/pages/Books.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BOOKS } from 'graphql/queries';
 import { useQuery } from '@apollo/client';
 import TokenContext from 'contexts/TokenContext';
@@ -6,12 +6,17 @@ import TokenContext from 'contexts/TokenContext';
 export default function Books() {
   let [token] = useContext(TokenContext);
   const {loading, error, data } = useQuery(BOOKS, {context:{headers:{"Authorization": token}}})
+  const [currentBooks, setCurrentBooks] = useState([])
+  const handleOnChange = (e) => {
+    setCurrentBooks(data?.allBooks.filter(b => b.title.indexOf(e.target.value) > -1))
+  }
+  useEffect(() => {setCurrentBooks(data?.allBooks)},[data])
   if(error) return 'Error on loading';
   return (
     <>
-    <div>List of books</div>
-    <p>Books:</p>
-    {loading ? 'Loading...' : data?.allBooks.map(b => <p key={b._id}>{b.title} - {b.description}</p>)}
+      <input type="text" onChange={handleOnChange} />
+      <p>Books:</p>
+      {loading ? 'Loading...' : currentBooks?.map(b => <p key={b._id}>{b.title} - {b.description}</p>)}
     </>
   );
 }
